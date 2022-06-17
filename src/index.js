d5f71227a0991c0120fc93b3023fa56861897462
Waqas Ahmad <waqaskanju@gmail.com>
2022-06-17 23:33:02 +0500

    dynamic ul

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
 import './style.css';
+
+const loadData = () => {
+  const scoreBoard = document.getElementById('score-board');
+  scoreBoard.innerHTML = '<li> Name 100 </li> <li> Name 80</li><li> Name 600</li><li> Name 50</li><li> Name 40</li>';
+};
+
+const refresh = document.querySelector('#refresh');
+refresh.addEventListener('click', loadData);
